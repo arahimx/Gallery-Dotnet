ec3debd7ac2dd2c1995c5d72955fc69c77094d44
Abdur Rahim <arawan0316@gmail.com>
2022-11-02 23:58:04 +0300

    Add project files.

---
+++ Random Images/Controllers/GalleryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Random_Images.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+
+        public ActionResult Index()
+        {
+            string salesFTPPath = "C:\\Users\\LENOVO\\source\\repos\\Random Images\\Random Images\\Content\\assets";
+            DirectoryInfo salesFTPDirectory = new DirectoryInfo(salesFTPPath);
+            IEnumerable<string> files = salesFTPDirectory.GetFiles()
+              .Where(f => f.Extension == ".gif" || f.Extension == ".wave" || f.Extension == ".mp4" || f.Extension == ".jpg" || f.Extension == ".jpeg" || f.Extension == ".png")
+              .OrderBy(f => f.Name)
+              .Select(f => f.FullName);
+            ViewBag.max = files.Count();
+            return View();
+        }
+        [HttpGet]
+        public JsonResult indexs()
+        {
+            string salesFTPPath = "C:\\Users\\LENOVO\\source\\repos\\Random Images\\Random Images\\Content\\assets";
+            DirectoryInfo salesFTPDirectory = new DirectoryInfo(salesFTPPath);
+            IEnumerable<string> files = salesFTPDirectory.GetFiles()
+              .Where(f => f.Extension == ".gif" || f.Extension == ".wave" || f.Extension == ".mp4" || f.Extension == ".jpg" || f.Extension == ".jpeg" || f.Extension == ".png")
+              .OrderBy(f => f.Name)
+              .Select(f => f.FullName);
+            return Json(files.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Random Images/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Random_Images.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = Server.MapPath("/Content/assets");
+            int fileCount = Directory.GetFiles(path, "*.*").Length;
+            ViewBag.max= fileCount;
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Index(int e)
+        //{
+        //    String fileName = "~/Content/Images/Image.png";
+        //    fileName = Server.MapPath(fileName);
+        //    if (File.Exists(fileName))
+        //    {
+        //        using (Bitmap bmp = new Bitmap(fileName))
+        //        using (Graphics g = Graphics.FromImage(bmp))
+        //        {
+        //            // do stuff here
+        //        }
+        //    }
+        //    return View()
+        //}
+    }
+}
